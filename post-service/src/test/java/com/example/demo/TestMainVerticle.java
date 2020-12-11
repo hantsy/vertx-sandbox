8397aeab57070a1c86ea89c808b040e62dd1c0bb
hantsy <hantsy@gmail.com>
2020-12-11 12:11:24 +0800

    test: add simple test codes

--- post-service/src/test/java/com/example/demo/TestMainVerticle.java
+++ post-service/src/test/java/com/example/demo/TestMainVerticle.java
@@ -4,12 +4,17 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,10 +22,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestMainVerticle {
 
     @BeforeEach
+    @DisplayName("Deploy MainVerticle")
     void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
         vertx.deployVerticle(new MainVerticle(), testContext.succeedingThenComplete());
     }
-
 
     // Repeat this test 3 times
     @RepeatedTest(3)
@@ -28,14 +33,46 @@ public class TestMainVerticle {
         HttpClient client = vertx.createHttpClient();
         client.request(HttpMethod.GET, 8888, "localhost", "/hello")
             .compose(req -> req.send().compose(HttpClientResponse::body))
-            .onComplete(testContext.succeeding(buffer -> testContext.verify(() -> {
-                assertThat(buffer.toString()).isEqualTo("Hello from my route");
-                testContext.completeNow();
-            })));
+            .onComplete(
+                testContext.succeeding(
+                    buffer -> testContext.verify(
+                        () -> {
+                            assertThat(buffer.toString()).isEqualTo("Hello from my route");
+                            testContext.completeNow();
+                        }
+                    )
+                )
+            );
     }
 
     @Test
+    @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+    @DisplayName("a simple test to verify deploy")
     void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
         testContext.completeNow();
+    }
+
+    static Stream<Arguments> testData() {
+        return Stream.of(
+            Arguments.of("complex object1", 4),
+            Arguments.of("complex object2", 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void test2(String obj, int count, Vertx vertx, VertxTestContext testContext) {
+        System.out.println(obj);
+        System.out.println(count);
+        // your test code
+        testContext.completeNow();
+    }
+
+    @AfterEach
+    @DisplayName("Check that the verticle is still there")
+    void lastChecks(Vertx vertx) {
+        assertThat(vertx.deploymentIDs())
+            .isNotEmpty()
+            .hasSize(1);
     }
 }
