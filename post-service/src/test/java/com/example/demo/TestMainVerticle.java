33e6cf61edb6000cf75657e0a58ab3f6d08262ae
hantsy <hantsy@gmail.com>
2020-12-11 11:48:31 +0800

    ci: add github actions workflow

--- post-service/src/test/java/com/example/demo/TestMainVerticle.java
+++ post-service/src/test/java/com/example/demo/TestMainVerticle.java
@@ -1,18 +1,37 @@
 package com.example.demo;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
     @BeforeEach
     void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-        vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+        vertx.deployVerticle(new MainVerticle(), testContext.succeedingThenComplete());
+    }
+
+
+    // Repeat this test 3 times
+    @RepeatedTest(3)
+    void http_server_check_response(Vertx vertx, VertxTestContext testContext) {
+        HttpClient client = vertx.createHttpClient();
+        client.request(HttpMethod.GET, 8888, "localhost", "/hello")
+            .compose(req -> req.send().compose(HttpClientResponse::body))
+            .onComplete(testContext.succeeding(buffer -> testContext.verify(() -> {
+                assertThat(buffer.toString()).isEqualTo("Hello from my route");
+                testContext.completeNow();
+            })));
     }
 
     @Test
