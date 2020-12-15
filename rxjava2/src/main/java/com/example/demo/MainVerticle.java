d5660502007e45623d9ea3b030febdc6e3711712
hantsy <hantsy@gmail.com>
2020-12-15 16:14:45 +0800

    chore: add json schema validation

--- rxjava2/src/main/java/com/example/demo/MainVerticle.java
+++ rxjava2/src/main/java/com/example/demo/MainVerticle.java
@@ -5,17 +5,29 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.reactivex.Completable;
+import io.vertx.core.Handler;
 import io.vertx.core.json.jackson.DatabindCodec;
-
+import io.vertx.ext.web.validation.BadRequestException;
+import io.vertx.ext.web.validation.builder.Bodies;
+import io.vertx.json.schema.SchemaRouterOptions;
+import io.vertx.json.schema.common.dsl.ObjectSchemaBuilder;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
 import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.ext.web.validation.ValidationHandler;
+import io.vertx.reactivex.json.schema.SchemaParser;
+import io.vertx.reactivex.json.schema.SchemaRouter;
 import io.vertx.reactivex.pgclient.PgPool;
 import io.vertx.sqlclient.PoolOptions;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static io.vertx.json.schema.common.dsl.Keywords.minLength;
+import static io.vertx.json.schema.common.dsl.Schemas.objectSchema;
+import static io.vertx.json.schema.common.dsl.Schemas.stringSchema;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -61,7 +73,7 @@ public class MainVerticle extends AbstractVerticle {
             .rxListen(8888)
             // Print the port
             .ignoreElement()
-        ;
+            ;
     }
 
     //create routes
@@ -72,7 +84,35 @@ public class MainVerticle extends AbstractVerticle {
         // register BodyHandler globally.
         //router.route().handler(BodyHandler.create());
         router.get("/posts").produces("application/json").handler(handlers::all);
-        router.post("/posts").consumes("application/json").handler(BodyHandler.create()).handler(handlers::save);
+
+        SchemaRouter schemaRouter = SchemaRouter.create(vertx, new SchemaRouterOptions());
+        SchemaParser schemaParser = SchemaParser.createDraft201909SchemaParser(schemaRouter);
+
+        ObjectSchemaBuilder bodySchemaBuilder = objectSchema()
+            .requiredProperty("title", stringSchema().with(minLength(5)))
+            .requiredProperty("content", stringSchema().with(minLength(10)));
+        ValidationHandler validation = ValidationHandler.newInstance(
+            ValidationHandler
+                .builder(schemaParser)
+                .body(Bodies.json(bodySchemaBuilder))
+                //.body(Bodies.formUrlEncoded(bodySchemaBuilder))
+                .build()
+        );
+
+        Handler<RoutingContext> validationFailureHandler = (RoutingContext rc) -> {
+            if (rc.failure() instanceof BadRequestException) {
+                rc.response()
+                    .setStatusCode(400)
+                    .end("validation failed.");
+            }
+        };
+
+        router.post("/posts").consumes("application/json")
+            .handler(validation)
+            .handler(BodyHandler.create())
+            .handler(handlers::save)
+            .failureHandler(validationFailureHandler);
+        
         router.get("/posts/:id").produces("application/json").handler(handlers::get).failureHandler(frc -> frc.response().setStatusCode(404).end());
         router.put("/posts/:id").consumes("application/json").handler(BodyHandler.create()).handler(handlers::update);
         router.delete("/posts/:id").handler(handlers::delete);
--- rxjava2/src/test/java/com/example/demo/TestMainVerticle.java
+++ rxjava2/src/test/java/com/example/demo/TestMainVerticle.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
+    private static final Logger LOGGER = Logger.getLogger(TestMainVerticle.class.getName());
 
     @BeforeEach
     void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
@@ -34,6 +38,25 @@ public class TestMainVerticle {
                     assertThat(response.statusCode()).isEqualTo(200);
                     assertThat(response.bodyAsJsonArray().size()).isEqualTo(2);
 
+                    testContext.completeNow();
+                }
+            );
+    }
+
+    @Test
+    void testPostValidation(Vertx vertx, VertxTestContext testContext) throws Throwable {
+        WebClient client = WebClient.create(new io.vertx.reactivex.core.Vertx(vertx));
+        client.post(8888, "localhost", "/posts")
+            .rxSendJson(PostForm.of("test", ""))
+            .subscribe(
+                response -> {
+                    var statusCode = response.statusCode();
+                    var body = response.bodyAsString();
+                    LOGGER.log(Level.INFO, "status code: {0}, body: {1}", new Object[] {
+                        statusCode, body
+                    });
+                    assertThat(statusCode).isEqualTo(400);
+                    assertThat(body).isEqualTo("validation failed.");
                     testContext.completeNow();
                 }
             );
