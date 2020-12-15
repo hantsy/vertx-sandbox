2eb87186d470d8f8b011ee46c108db1523c20361
hantsy <hantsy@gmail.com>
2020-12-15 11:57:03 +0800

    feat: rxjava 2

---
+++ rxjava2/src/main/java/com/example/demo/MainVerticle.java
@@ -0,0 +1,102 @@
+package com.example.demo;
+
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.reactivex.Completable;
+import io.vertx.core.json.jackson.DatabindCodec;
+
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.pgclient.PgPool;
+import io.vertx.sqlclient.PoolOptions;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainVerticle extends AbstractVerticle {
+
+    private final static Logger LOGGER = Logger.getLogger(MainVerticle.class.getName());
+
+    static {
+        LOGGER.info("Customizing the built-in jackson ObjectMapper...");
+        var objectMapper = DatabindCodec.mapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+        objectMapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
+
+        JavaTimeModule module = new JavaTimeModule();
+        objectMapper.registerModule(module);
+    }
+
+    @Override
+    public Completable rxStart() {
+        LOGGER.log(Level.INFO, "Starting HTTP server...");
+        //setupLogging();
+
+        //Create a PgPool instance
+        var pgPool = pgPool();
+
+        //Creating PostRepository
+        var postRepository = PostRepository.create(pgPool);
+
+        //Creating PostHandler
+        var postHandlers = PostsHandler.create(postRepository);
+
+        // Initializing the sample data
+        var initializer = DataInitializer.create(pgPool);
+        initializer.run();
+
+        // Configure routes
+        var router = routes(postHandlers);
+
+        // Create the HTTP server
+        return vertx.createHttpServer()
+            // Handle every request using the router
+            .requestHandler(router)
+            // Start listening
+            .rxListen(8888)
+            // Print the port
+            .ignoreElement()
+        ;
+    }
+
+    //create routes
+    private Router routes(PostsHandler handlers) {
+
+        // Create a Router
+        Router router = Router.router(vertx);
+        // register BodyHandler globally.
+        //router.route().handler(BodyHandler.create());
+        router.get("/posts").produces("application/json").handler(handlers::all);
+        router.post("/posts").consumes("application/json").handler(BodyHandler.create()).handler(handlers::save);
+        router.get("/posts/:id").produces("application/json").handler(handlers::get).failureHandler(frc -> frc.response().setStatusCode(404).end());
+        router.put("/posts/:id").consumes("application/json").handler(BodyHandler.create()).handler(handlers::update);
+        router.delete("/posts/:id").handler(handlers::delete);
+
+        router.get("/hello").handler(rc -> rc.response().end("Hello from my route"));
+
+        return router;
+    }
+
+    private PgPool pgPool() {
+        PgConnectOptions connectOptions = new PgConnectOptions()
+            .setPort(5432)
+            .setHost("localhost")
+            .setDatabase("blogdb")
+            .setUser("user")
+            .setPassword("password");
+
+        // Pool Options
+        PoolOptions poolOptions = new PoolOptions().setMaxSize(5);
+
+        // Create the pool from the data object
+        PgPool pool = PgPool.pool(vertx, connectOptions, poolOptions);
+
+        return pool;
+    }
+
+}
