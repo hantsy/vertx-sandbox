0f17d1e2d171b81ce14a56396eb33bbc00fa1e26
Hantsy Bai <hantsy@gmail.com>
2021-06-28 11:00:31 +0800

    chore: clean testing codes

--- post-service-cdi/src/test/java/com/example/demo/TestMainVerticle.java
+++ post-service-cdi/src/test/java/com/example/demo/TestMainVerticle.java
@@ -1,7 +1,75 @@
 package com.example.demo;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.spi.VerticleFactory;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnableAutoWeld
+@AddPackages(DemoApplication.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
+    private final static Logger LOGGER = Logger.getLogger(TestMainVerticle.class.getName());
+
+    @Inject
+    Instance<Object> context;
+
+    Vertx vertx;
+
+    @BeforeAll
+    public void setupAll(VertxTestContext testContext) {
+        vertx = context.select(Vertx.class).get();
+        var factory = context.select(VerticleFactory.class).get();
+        vertx.deployVerticle(factory.prefix() + ":" + MainVerticle.class.getName())
+            .onSuccess(id -> {
+                LOGGER.info("deployed:" + id);
+                testContext.completeNow();
+            });
+    }
+
+    @Test
+    public void testVertx(VertxTestContext testContext) {
+        assertThat(vertx).isNotNull();
+        testContext.completeNow();
+    }
+
+
+    @Test
+    void testGetAll(VertxTestContext testContext) {
+        LOGGER.log(Level.INFO, "running test: {0}", "testGetAll");
+        var options = new HttpClientOptions()
+            .setDefaultPort(8888);
+        var client = vertx.createHttpClient(options);
+
+        client.request(HttpMethod.GET, "/posts")
+            .flatMap(req -> req.send().flatMap(HttpClientResponse::body))
+            .onSuccess(
+                buffer -> testContext.verify(
+                    () -> {
+                        LOGGER.log(Level.INFO, "response buffer: {0}", new Object[]{buffer.toString()});
+                        assertThat(buffer.toJsonArray().size()).isGreaterThan(0);
+                        testContext.completeNow();
+                    }
+                )
+            )
+            .onFailure(e -> LOGGER.log(Level.ALL, "error: {0}", e.getMessage()));
+    }
+
 }
