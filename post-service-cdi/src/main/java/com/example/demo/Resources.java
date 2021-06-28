445463260954dc5eb43a5d0ec332868ba810b586
Hantsy Bai <hantsy@gmail.com>
2021-06-28 12:06:30 +0800

    chore: clean codes

--- post-service-cdi/src/main/java/com/example/demo/Resources.java
+++ post-service-cdi/src/main/java/com/example/demo/Resources.java
@@ -7,11 +7,14 @@ import io.vertx.pgclient.PgPool;
 import io.vertx.sqlclient.PoolOptions;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class Resources {
+    private final static Logger LOGGER = Logger.getLogger(Resources.class.getName());
 
     @Produces
     @Singleton
@@ -35,7 +38,12 @@ public class Resources {
 
         // Create the pool from the data object
         PgPool pool = PgPool.pool(vertx, connectOptions, poolOptions);
-        
+
         return pool;
+    }
+
+    public void disposesPgPool(@Disposes PgPool pgPool) {
+        LOGGER.info("disposing PgPool...");
+        pgPool.close().onSuccess(v -> LOGGER.info("PgPool is closed successfully."));
     }
 }
