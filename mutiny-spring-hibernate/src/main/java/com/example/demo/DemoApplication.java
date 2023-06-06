5d5225b8127da718b0b7d7c0521644948ec87efe
hantsy <hantsy@gmail.com>
2023-06-06 10:12:29 +0800

    chore: clean codes

--- mutiny-spring-hibernate/src/main/java/com/example/demo/DemoApplication.java
+++ mutiny-spring-hibernate/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.Persistence;
+import jakarta.persistence.Persistence;
 
 @Configuration
 @ComponentScan
